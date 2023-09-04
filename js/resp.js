5ddc76ec2a2f1a32c6dd4fead2a813c7303458d3
akshay-m-47 <132677904+akshay-m-47@users.noreply.github.com>
2023-09-04 10:57:30 +0530

    Initial commit

---
+++ js/resp.js
@@ -0,0 +1,27 @@
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navlist = document.querySelector('.nav-list');
+navright = document.querySelector('.nav-right');
+
+
+burger.addEventListener('click', ()=>{
+    navright.classList.toggle('v-class');
+    navlist.classList.toggle('v-class');
+    navbar.classList.toggle('h-nav');
+})
+ a.addEventListener('click',function(e){
+    // location.replace('https://www.google.com/')
+    // window.location='https://www.google.com/'
+    window.open('https://wa.me/qr/NKAHH2VXWVKPH1')
+ })
+ b.addEventListener('click',function(e){
+    // location.replace('https://www.google.com/')
+    // window.location='https://www.google.com/'
+    window.open('https://twitter.com/AkshayMule78653')
+ })
+ c.addEventListener('click',function(e){
+    // location.replace('https://www.google.com/')
+    // window.location='https://www.google.com/'
+    window.open('https://www.instagram.com/akshay_muley_/')
+ })
+
